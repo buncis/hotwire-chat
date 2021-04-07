e45ebdbfc90e9cad94a8d2611f176b54c7af8377
buncis <buncismamen@gmail.com>
2021-04-08 01:40:19 +0700

    add turbo stream for message

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :messages
   end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'rooms#index'
 end
